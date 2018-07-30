71a69a4af8a839d0777b531e5bd2d5e54c84361c
sirjmmontalbo <36910105+sirjmmontalbo@users.noreply.github.com>
2018-07-30 20:52:11 +0800

    Add files via upload
    This is a basic Registration HTML

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration Form</title>
+</head>
+<body>
+<form>
+	<table>
+
+		<tr>
+			<td>
+				Name:
+			</td>
+			<td>
+				<input type="text" placeholder="name" name="">
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Password:
+			</td>
+			<td>
+				<input type="password" placeholder="Password" name="">
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Gender:
+			</td>
+			<td>
+				<input type="radio" name="Gender" >Male
+				<input type="radio" name="Gender">Female
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Email:
+			</td>
+			<td>
+				<input type="Email" placeholder="Email" name="">
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				Phone no:
+			</td>
+			<td>
+				<select>
+					<option>0917</option>
+					<option>0915</option>
+					<option>0920</option>
+				</select>
+				<input type="Phone" placeholder="********" name="">
+			</td>
+		</tr>
+		<tr>
+			<td>
+				<input type="Submit" value="Submit" name="">
+			</td>
+		</tr>
+
+		<tr>
+			<td>
+				
+			</td>
+		</tr>
+
+	</table>
+</form>
+</body>
+</html>
